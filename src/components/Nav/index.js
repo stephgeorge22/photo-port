2bf8028c5bc39c76208b23438a5971da98b7dfd3
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-08-31 20:44:31 -0400

    render the modal

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
@@ -10,11 +10,9 @@ function Nav(props) {
     setContactSelected
   } = props;
 
-
-  const handleClick = (item) => {
-    console.log(item);
-    return item;
-  };
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
@@ -35,7 +33,8 @@ function Nav(props) {
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${currentCategory.name === category.name && !contactSelected && `navActive`
+              className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && `navActive`
                 }`}
               key={category.name}
             >
